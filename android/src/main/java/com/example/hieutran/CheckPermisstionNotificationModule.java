2e4f251307e1149228fbe6baff8261e37f514838
hieutran <hanhieu@ononpay.com>
2018-06-26 00:47:43 +0700

    check permisstion

--- android/src/main/java/com/example/hieutran/CheckPermisstionNotificationModule.java
+++ android/src/main/java/com/example/hieutran/CheckPermisstionNotificationModule.java
@@ -3,6 +3,12 @@ package com.example.hieutran.checkPermisstionNotification;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
+import android.app.AppOpsManager;
+import android.content.pm.ApplicationInfo;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
@@ -12,6 +18,9 @@ import com.facebook.react.bridge.ReactMethod;
 public class CheckPermisstionNotificationModule extends ReactContextBaseJavaModule {
 
     private ReactContext reactContext;
+    private static final String CHECK_OP_NO_THROW = "checkOpNoThrow";
+    private static final String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
+
 
     public CheckPermisstionNotificationModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -34,6 +43,44 @@ public class CheckPermisstionNotificationModule extends ReactContextBaseJavaModu
         i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
         reactContext.startActivity(i);
+    }
+    @ReactMethod
+    public boolean checkPermisstion() {
+
+        AppOpsManager mAppOps = (AppOpsManager) reactContext.getSystemService(Context.APP_OPS_SERVICE);
+
+        ApplicationInfo appInfo = reactContext.getApplicationInfo();
+
+        String pkg = reactContext.getApplicationContext().getPackageName();
+
+        int uid = appInfo.uid;
+
+        Class appOpsClass = null; /* Context.APP_OPS_MANAGER */
+
+        try {
+
+            appOpsClass = Class.forName(AppOpsManager.class.getName());
+
+            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE, String.class);
+
+            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+            int value = (int)opPostNotificationValue.get(Integer.class);
+
+            return ((int)checkOpNoThrowMethod.invoke(mAppOps,value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return false;
+
     }
     //endregion
 }
